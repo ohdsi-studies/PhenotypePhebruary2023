c1b78cd6e239319d940b06c4690974a00c2b54df
Gowtham Rao <rao@ohdsi.org>
2023-02-07 17:47:46 -0500

    Cohorts for Neurofibromatosis
    https://forums.ohdsi.org/t/phenotype-phebruary-2023-p11-neurofibromatosis-type-1-with-optical-pathway-glioma/18236

---
+++ extras/examples/dataSourceInformation.R
@@ -0,0 +1,57 @@
+source(Sys.getenv("startUpScriptLocation"))
+
+######
+executeOnMultipleDataSources <- function(x) {
+  library(magrittr)
+  if (x$generateCohortTableName) {
+    cohortTableName <- paste0(stringr::str_squish(x$projectCode),
+                              stringr::str_squish(x$sourceId))
+  }
+  
+  extraLog <- (
+    paste0(
+      "Running ",
+      x$databaseName,
+      " (",
+      x$sourceId,
+      ") on ",
+      x$sourceId,
+      "\n     server: ",
+      x$runOn,
+      " (",
+      x$serverFinal,
+      ")",
+      "\n     cdmDatabaseSchema: ",
+      x$cdmDatabaseSchema,
+      "\n     cohortDatabaseSchema: ",
+      x$cohortDatabaseSchema
+    )
+  )
+  
+  # Details for connecting to the server:
+  connectionDetails <-
+    DatabaseConnector::createConnectionDetails(
+      dbms = x$dbms,
+      server = x$server,
+      user = keyring::key_get(service = x$userService),
+      password =  keyring::key_get(service = x$passwordService),
+      port = x$port
+    )
+  # The name of the database schema where the CDM data can be found:
+  cdmDatabaseSchema <- x$cdmDatabaseSchema
+  vocabDatabaseSchema <- x$vocabDatabaseSchema
+  cohortDatabaseSchema <- x$cohortDatabaseSchema
+  
+  databaseId <- x$databaseId
+  
+  phenotypePhebruary2023::execute(
+    connectionDetails = connectionDetails,
+    cdmDatabaseSchema = cdmDatabaseSchema,
+    cohortDatabaseSchema = cohortDatabaseSchema,
+    cohortTable = cohortTableName,
+    verifyDependencies = x$verifyDependencies,
+    outputFolder = x$outputFolder,
+    databaseId = databaseId,
+    extraLog = extraLog
+  )
+}
